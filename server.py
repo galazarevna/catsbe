c019c7abf6c0222e3882998025fc311f387bbead
galazarevna <foringela@gmail.com>
2022-05-08 00:04:07 -0700

    Add zip_code api for cities. Add show all users to react. Updtae babel src.

--- server.py
+++ server.py
@@ -1,13 +1,20 @@
-"""Server for movie ratings app."""
+"""Server for catsbe app."""
+import os
 
-from flask import Flask, render_template, request, flash, session, redirect
+import requests
+from flask import Flask, render_template, request, flash, session, redirect, jsonify
 from jinja2 import StrictUndefined
+from requests.auth import HTTPBasicAuth
 
 from model import connect_to_db, User
 
 app = Flask(__name__)
 app.secret_key = "dev"
 app.jinja_env.undefined = StrictUndefined
+
+ZIP_CODE_API = os.getenv('ZIP_CODE_API', None)
+EMAIL = os.getenv('EMAIL', None)
+PASSWORD = os.getenv('PASSWORD', None)
 
 
 @app.route("/")
@@ -55,6 +62,20 @@ def all_users():
 #
 #     return render_template("profile_page.html", user=user)
 
+def get_city(zip_code):
+    city_req = requests.get(
+        f"https://service.zipapi.us/zipcode/{zip_code}?X-API-KEY={ZIP_CODE_API}&fields=geolocation,population",
+        auth=HTTPBasicAuth(EMAIL, PASSWORD))
+    print("city_req=", city_req)
+    response = city_req.json()
+    city = zip_code
+    try:
+        city = response["data"]["city"]
+    except KeyError as e:
+        pass
+    return city
+
+
 @app.route("/user.json")
 def user():
     if "user_id" in session:
@@ -62,8 +83,19 @@ def user():
         user = User.get_by_id(user_id)
         print(user)
         user_as_dict = user.as_dict()
+        user_as_dict["city"] = get_city(user_as_dict["zip_code"])
 
         return user_as_dict
+
+
+@app.route("/users.json")
+def users():
+    all_users = User.all_users()
+    users_list = []
+    for user_obj in all_users:
+        users_list.append(user_obj.as_dict())
+    print(users_list)
+    return jsonify({"users": users_list})
 
 
 # @app.route("/movies")
--- static/js/userProfile.jsx
+++ static/js/userProfile.jsx
@@ -4,6 +4,7 @@ function UserProfile() {
     const [about_me, setAboutMe] = React.useState("");
     const [zip_code, setZipCode] = React.useState(0);
     const [last_seen, setLastSeen] = React.useState("");
+    const [city, setCity] = React.useState("");
 
 
     React.useEffect(() => {
@@ -15,6 +16,7 @@ function UserProfile() {
             setZipCode(result.zip_code);
             setAboutMe(result.about_me);
             setLastSeen(result.last_seen);
+            setCity(result.city);
         });
       }, []);
 
@@ -25,11 +27,58 @@ function UserProfile() {
         <img src="/static/img/default.jpg" alt="profile" />
         <p>Name: {user}</p>
         <p>About me: {about_me}</p>
-        <p>From: <a href={location}> {zip_code}</a></p>
+        <p>From: <a href={location}> {city}</a></p>
         <p>Last seen: {last_seen}</p>
 
         </div>
     );
 }
 
-ReactDOM.render(<UserProfile />, document.querySelector('#container'));
+
+function User(props) {
+  return (
+    <div className="user">
+      <h2>Name: {props.username}</h2>
+      <img src="/static/img/default.jpg" alt="profile" />
+      <h2>About me: {props.about_me}</h2>
+      <h2>From: {props.zip_code}</h2>
+      <h2>Last seen: {props.last_seen}</h2>
+    </div>
+  );
+}
+
+function AllUsersContainer(props) {
+  const [users, setUsers] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch('/users.json')
+    .then((response) => response.json())
+    .then((data) => setUsers(data.users))
+  }, [])
+
+  const all_users = [];
+
+  for (const user of users) {
+    all_users.push(
+      <User
+        username={user.username}
+        about_me={user.about_me}
+        zip_code={user.zip_code}
+        last_seen={user.last_seen}
+        imgUrl="/static/img/default.jpg"
+        key={user.user_id}
+      />,
+    );
+  }
+
+  return (
+    <React.Fragment>
+      <UserProfile />
+      <h2>All Users</h2>
+      <div className="grid">{all_users}</div>
+    </React.Fragment>
+  );
+}
+
+
+ReactDOM.render(<AllUsersContainer />, document.querySelector('#container'));
